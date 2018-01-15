58ee3ad37ef4b2b164bf2a7d71b381d8232abeb8
Kaiz0r <kylebell68@gmail.com>
2018-01-15 11:59:53 +0000

    First upload of dchk, the directory check tool.

---
+++ dchk.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+dlpath = '/home/kaiz0r/.wine/drive_c/Deus Ex GOTY/Cache/'
+fileext = ".uxx"
+dchkstr = "bAdmin"
+
+foundnum = 0
+foundstr = ""
+
+try:
+	dlpath = sys.argv[1]
+except IndexError:
+	pass
+
+try:
+	fileext = sys.argv[2]
+except IndexError:
+	pass
+	
+try:
+	dchkstr = sys.argv[3]
+except IndexError:
+	pass
+	
+def dchk():
+	global dlpath, dchkstr, fileext, foundnum, foundstr
+	for file in os.listdir(dlpath):
+		filename = os.fsdecode(file)
+		if filename.endswith(fileext):
+			print("Opening "+filename)
+			with open(dlpath+filename, "rb") as fl:
+				cont = str(fl.read(), "latin-1")
+				if dchkstr in cont:
+					print("Found in "+filename)
+					foundnum += 1
+					foundstr = foundstr+"\n"+filename
+				else:
+					print("Nope...")
+			
+
+print("Running script...")
+print("Checking "+dlpath+" *"+fileext+" for "+dchkstr+"...")
+dchk()
+print(str(foundnum)+" files matched!\n"+foundstr)
+input("Script complete. Press enter to close.")
